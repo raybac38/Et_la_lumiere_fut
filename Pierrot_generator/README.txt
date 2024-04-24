1b9e57fc1b1c116dc1df492abc4e885716eea2bb
Medina Pierre <medinap@etu.univ-grenoble-alpes.fr>
2024-04-24 11:36:23 +0200

    Maj 24/04 v8

--- Pierrot_generator/README.txt
+++ Pierrot_generator/README.txt
@@ -1,8 +1,10 @@
 final_dens = (X_SIZE*Y_SIZE)*DENSITE;
 
-V1 n'a jamais marché
+V1 na jamais marché
 V2 tout le terrain
 V3 enleve seulement les intersections
 V4 intersection, sans demande de densité
-V5 generation avec demande de densité et supression des cracteres inutiles
+V5 generation avec demande de densité et supression des caracteres inutiles
 V6 ajout alternance des \ et /
+V7 ajout fun verif_tabl : suppression des # seuls et #-#-#-# (avec moins de deux routes qui connecte le bloc) car cela renvoit une erreur logique
+V8 programme avec densite longueur largueur en argument
---
+++ Pierrot_generator/terrain_generatorV8.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 1000
+
+typedef struct {
+    int size_max_x;
+    int size_max_y;
+    char tab[MAX_SIZE][MAX_SIZE];
+} Terrain;
+
+void imprimer_tableau(FILE *fichier, Terrain tablo, int x, int y) {
+    int x0, y0;
+
+    for (x0 = 0; x0 < x; x0++) {
+        for (y0 = 0; y0 < y; y0++) {
+            fprintf(fichier, "%c ", tablo.tab[x0][y0]);
+        }
+        fprintf(fichier, "\n");
+    }
+}
+
+void generer_terrain(Terrain *tablo, double densite) {
+    int x0, y0;
+
+    // Initialisation avec des caractères par défaut
+    for (x0 = 0; x0 < MAX_SIZE; x0++) {
+        for (y0 = 0; y0 < MAX_SIZE; y0++) {
+            tablo->tab[x0][y0] = ' ';
+        }
+    }
+
+    for (x0 = 0; x0 < tablo->size_max_x; x0++) {
+        for (y0 = 0; y0 < tablo->size_max_y; y0++) {
+            if ((x0 % 2) == 1 && (y0 % 2) == 1) {
+                tablo->tab[x0][y0] = '#';
+            } else if ((x0 % 2) == 1 && (y0 % 2) == 0) {
+                tablo->tab[x0][y0] = '-';
+            } else if ((x0 % 2) == 0 && (y0 % 2) == 1) {
+                tablo->tab[x0][y0] = '|';
+            } else if ((x0 % 2) == 0 && (y0 % 2) == 0 && x0 != tablo->size_max_x - 1 && y0 != tablo->size_max_y - 1) {
+                if ((rand()%2) == 0)  {
+                    tablo->tab[x0][y0] = '/';     
+                    }
+                else {
+                    tablo->tab[x0][y0] = '\\'; 
+                    }
+            }
+        }
+    }
+
+    int total_intersections = (tablo->size_max_x / 2) * (tablo->size_max_y / 2);
+    int nb_intersections_hashtag = (int)(densite * total_intersections);
+
+    srand(time(NULL));
+
+    for (int i = 0; i < nb_intersections_hashtag; i++) {
+        int x, y;
+        do {
+            x = rand() % (tablo->size_max_x / 2) * 2 + 1;
+            y = rand() % (tablo->size_max_y / 2) * 2 + 1;
+        } while (tablo->tab[x][y] != '#');
+        
+        tablo->tab[x][y] = ' ';
+    }
+
+    // Supprimer les caractères qui ne sont pas entre deux '#' ou entre un '#' et le bord du terrain
+    for (x0 = 0; x0 < tablo->size_max_x; x0++) {
+        for (y0 = 0; y0 < tablo->size_max_y; y0++) {
+            if (tablo->tab[x0][y0] == '-' || tablo->tab[x0][y0] == '|' || tablo->tab[x0][y0] == '\\' || tablo->tab[x0][y0] == '/') {
+                // Vérifier si le caractère est entre deux '#' dans la même rangée
+                if ((y0 - 1 >= 0 && tablo->tab[x0][y0 - 1] == '#') && (y0 + 1 < tablo->size_max_y && tablo->tab[x0][y0 + 1] == '#')) {
+                    continue;
+                }
+                // Vérifier si le caractère est entre deux '#' dans la même colonne
+                if ((x0 - 1 >= 0 && tablo->tab[x0 - 1][y0] == '#') && (x0 + 1 < tablo->size_max_x && tablo->tab[x0 + 1][y0] == '#')) {
+                    continue;
+                }
+                if ((x0 - 1 >= 0 && y0 - 1 >= 0) && tablo->tab[x0-1][y0-1] == '#') {
+                    if ((x0 + 1 < tablo->size_max_x) && (y0+1 < tablo->size_max_y) && tablo->tab[x0+1][y0+1] == '#') {
+                        continue;
+                    }
+                }
+  
+                // Si aucune des conditions ci-dessus n'est remplie, supprimer le caractère
+                tablo->tab[x0][y0] = ' ';
+            }
+        }
+    }
+}
+
+void verif_tabl(Terrain *tablo, double densite) {
+    int x0, y0;
+     for (x0 = 0; x0 < tablo->size_max_x; x0++) {
+        for (y0 = 0; y0 < tablo->size_max_y; y0++) {
+            
+            if (x0 > 0 && y0 > 0 && x0 < tablo->size_max_x && y0 < tablo->size_max_x) {
+                if(tablo->tab[x0][y0] == '#' && tablo->tab[x0-1][y0-1] == ' ' && tablo->tab[x0-1][y0] == ' ') {
+                    if(tablo->tab[x0][y0-1] == ' ' &&  tablo->tab[x0+1][y0] ==' ' && tablo->tab[x0][y0+1] == ' ' && tablo->tab[x0+1][y0+1] == ' ') {
+                        tablo->tab[x0][y0] = ' ';
+                    }
+                }
+            }
+//            if (x0 > 0 && y0 > 0 && x0 < ((tablo->size_max_x)-8)) && y0 < ((tablo->size_max_x)-8) {
+//                if(tablo->tab[x0][y0] = '#' && tablo->tab[x0][y0+2] = '#' tablo->tab[x0][y0+4] = '#' tablo->tab[x0][y0+6] = '#') {
+//                    if (tablo->tab[x0-1][y0])
+//                }
+//            }
+
+            
+
+        }
+    }
+}
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s <taille_x> <taille_y> <densite>\n", argv[0]);
+        return 1;
+    }
+
+    int x = atoi(argv[1]); // Convertit le premier argument en entier
+    int y = atoi(argv[2]); // Convertit le deuxième argument en entier
+    double densite = atof(argv[3]); // Convertit le troisième argument en double
+
+    Terrain tablo;
+
+    FILE *fichier;
+
+    fichier = fopen("et_la_lumiere_futV2.txt", "w");
+
+    if (fichier == NULL) {
+        printf("Erreur lors de l'ouverture du fichier.");
+        return 1;
+    }
+
+    fprintf(fichier, "Largeur : %d\n", x);
+    fprintf(fichier, "Hauteur : %d\n", y);
+    fprintf(fichier, "Densite : %.2lf\n\n", densite);
+    fprintf(fichier, "----------------------------------------------------------------------------------- \n Generation du terrain: \n");
+
+    tablo.size_max_x = x;
+    tablo.size_max_y = y;
+
+    generer_terrain(&tablo, densite); // Densité de caractères '#' selon la densité spécifiée
+    verif_tabl(&tablo, densite);
+
+    imprimer_tableau(fichier, tablo, tablo.size_max_x, tablo.size_max_y);
+
+    fclose(fichier);
+    return 0;
+}
